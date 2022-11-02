d754ed97e7162f32d2a7d199dc42d183c0cb19d2
sfjbs <navyadintyala@gmail.com>
2022-11-02 13:26:14 +0000

    commit

---
+++ Navyasree/General/demo/src/main/java/com/example/demo/GeneralApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneralApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneralApplication.class, args);
+		System.out.println("Hello");
+	}
+
+}
