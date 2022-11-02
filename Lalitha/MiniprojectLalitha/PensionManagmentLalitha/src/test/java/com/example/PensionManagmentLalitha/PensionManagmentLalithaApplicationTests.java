1d4eb90a8db970df6283f909547b483acc076f9f
LalithaKakarala <117061996+LalithaKakarala@users.noreply.github.com>
2022-11-02 14:31:06 +0000

    Miniproject Pension

---
+++ Lalitha/MiniprojectLalitha/PensionManagmentLalitha/src/test/java/com/example/PensionManagmentLalitha/PensionManagmentLalithaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PensionManagmentLalitha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PensionManagmentLalithaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
