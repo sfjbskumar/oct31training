da474d1c7cfaa187cc629ff5da9a82b030dfd900
Priyanshu Prakash <priyanshuhay123@gmail.com>
2022-11-02 06:04:06 +0000

    2nd day commit

---
+++ PriyanshuPrakash/Assignments/pension-management-System/src/main/java/com/example/pension/management/System/PensionManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.pension.management.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PensionManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PensionManagementSystemApplication.class, args);
+	}
+
+}
---
+++ PriyanshuPrakash/Assignments/pension-management-System/src/test/java/com/example/pension/management/System/PensionManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pension.management.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PensionManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
