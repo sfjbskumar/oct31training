2b3590b293211c2389b0d0f97f79a886a3cd0234
sfjbs <msakhila02@gmail.com>
2022-11-02 06:33:29 +0000

    commit

---
+++ Akhila/Day 3/src/test/java/com/calculator/service/Calculatorapplication/CalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.calculator.service.Calculatorapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
