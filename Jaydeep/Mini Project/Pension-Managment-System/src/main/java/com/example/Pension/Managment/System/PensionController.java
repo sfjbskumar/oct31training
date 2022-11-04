1311052798aceb7fa47626c2a66dbacd358aa3de
sfjbs <jaydeepbadadare@gmail.com>
2022-11-04 11:59:48 +0000

    Assignment mini project controller commit

--- Jaydeep/Mini Project/Pension-Managment-System/src/main/java/com/example/Pension/Managment/System/PensionController.java
+++ Jaydeep/Mini Project/Pension-Managment-System/src/main/java/com/example/Pension/Managment/System/PensionController.java
@@ -1,7 +1,63 @@
 package com.example.Pension.Managment.System;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 public class PensionController {
+
+    private Map<String,Pension> pensionMap= new HashMap<String,Pension>();
+
+    @GetMapping("/get/PensionName")
+    public Pension getName(@RequestParam String name){
+        Pension result= pensionMap.get(name);
+        return result;
+    }
+
+    @GetMapping("/get/all")
+    public Map<String,Pension> getName(){
+        return pensionMap;
+    }
+
+    @PostMapping("save/PensionName")
+    public void SavePensionName(@RequestBody Pension pension){
+        String name= pension.getName();
+        pensionMap.put(name,pension);
+    }
+    @PutMapping("/update/PensionName")
+    public Pension updatePensionName(@RequestParam String name,@RequestParam int id){
+        Pension result= pensionMap.get(name);
+        result.setId(id);
+        pensionMap.put(name,result);
+        return result;
+    }
+
+
+    @DeleteMapping("remove/PensionName")
+    public void deletePensionName(@RequestParam String name){
+        pensionMap.remove(name);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
+
