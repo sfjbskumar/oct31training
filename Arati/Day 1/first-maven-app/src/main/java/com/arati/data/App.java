b75783a256d7e66b148bf73dc586c28373fa3b1a
sfjbs <aratijaware2@gmail.com>
2022-11-06 15:30:00 +0000

    first commit

---
+++ Arati/Day 1/first-maven-app/src/main/java/com/arati/data/App.java
@@ -0,0 +1,13 @@
+package com.arati.data;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
