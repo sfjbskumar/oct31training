750c68bbe96106b9f92cbf288e377e9b86add303
sfjbs <navyadintyala@gmail.com>
2022-11-09 08:36:59 +0000

    commit Assignment

---
+++ Navyasree/Pension/PensionManagement/src/main/java/com/example/demo/PensionManagemetSys.java
@@ -0,0 +1,112 @@
+package com.example.demo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PensionManagemetSys {
+   private int id;
+    private  String name;
+    private  String age;
+   private  int balanceamount ;
+    private int phone;
+    private String empstatus;
+    private String pensionstatus;
+
+    private String  pensionmmyy;
+    private int installment;
+
+    public PensionManagemetSys(){
+
+    }
+
+
+
+    public PensionManagemetSys(int id, String name, String age, int balanceamount, int phone, String empstatus, String pensionstatus, String pensionmmyy, int installment) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.balanceamount = balanceamount;
+        this.phone = phone;
+        this.empstatus = empstatus;
+        this.pensionstatus = pensionstatus;
+        this.pensionmmyy = pensionmmyy;
+        this.installment = installment;
+    }
+
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public int getBalanceamount() {
+        return balanceamount;
+    }
+
+    public void setBalanceamount(int balanceamount) {
+        this.balanceamount = balanceamount;
+    }
+
+    public Integer getPhone() {
+        return phone;
+    }
+
+    public void setPhone(int phone) {
+        this.phone= phone;
+    }
+
+    public String getEmpstatus() {
+        return empstatus;
+    }
+
+    public void setEmpstatus(String empstatus) {
+        this.empstatus = empstatus;
+    }
+
+    public String getPensionstatus() {
+        return pensionstatus;
+    }
+
+    public void setPensionstatus(String pensionstatus) {
+        this.pensionstatus = pensionstatus;
+    }
+
+    public String getPensionmmyy() {
+        return pensionmmyy;
+    }
+
+    public void setPensionmmyy(String pensionmmyy) {
+        this.pensionmmyy = pensionmmyy;
+    }
+
+    public int getInstallment() {
+        return installment;
+    }
+
+    public void setInstallment(int installment) {
+        this.installment = installment;
+    }
+
+
+}
