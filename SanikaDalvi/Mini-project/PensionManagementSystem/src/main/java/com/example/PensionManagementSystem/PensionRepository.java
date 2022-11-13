28ddec2f98da599fef6e35a3970a0199bf84cf2a
sfjbs <sanika.dalvi2000@gmail.com>
2022-11-13 17:41:30 +0000

    Dockerization done

--- SanikaDalvi/Mini-project/PensionManagementSystem/src/main/java/com/example/PensionManagementSystem/PensionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.PensionManagementSystem;
-
-import com.example.PensionManagementSystem.model.Pension;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PensionRepository extends CrudRepository<Pension,Integer>{
-}
