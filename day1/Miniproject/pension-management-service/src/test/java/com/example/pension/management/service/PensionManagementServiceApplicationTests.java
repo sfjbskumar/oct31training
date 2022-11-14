740f1ca345415bbbd4bbf18abcc34fac45ebbb8e
naz-22 <117144600+naz-22@users.noreply.github.com>
2022-11-14 06:49:25 +0000

    Add files via upload

---
+++ day1/Miniproject/pension-management-service/src/test/java/com/example/pension/management/service/PensionManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pension.management.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PensionManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
