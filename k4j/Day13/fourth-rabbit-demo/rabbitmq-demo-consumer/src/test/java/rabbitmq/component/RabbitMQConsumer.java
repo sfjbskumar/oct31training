a3a0d72cded408a5bd89c7c7896bc6b7f81a8a88
kumar4java <kumar4javatraining@gmail.com>
2022-11-15 11:28:00 +0530

    Day13

---
+++ k4j/Day13/fourth-rabbit-demo/rabbitmq-demo-consumer/src/test/java/rabbitmq/component/RabbitMQConsumer.java
@@ -0,0 +1,39 @@
+package com.rama.rabbitmq.component;
+
+import com.rama.rabbitmq.model.Employee;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RabbitMQConsumer {
+
+  /*
+  If you are using boot, you can simply add a Jackson2JsonMessageConverter
+  @Bean to the configuration and it will be automatically wired into the listener (
+   */
+ /* @Value("${rabbitmq.queue.name}")
+  String queueName;
+
+  @Value("${rabbitmq.exchange.name}")
+  String exchange;
+
+  @Value("${rabbitmq.routingkey.name}")
+  private String routingkey;*/
+  @Bean
+  public Jackson2JsonMessageConverter converter() {
+    return new Jackson2JsonMessageConverter();
+  }
+
+  /*public void send(Employee company) {
+    rabbitTemplate.convertAndSend(exchange, routingkey, company);
+    System.out.println("Send msg = " + company);
+
+  }*/
+  @RabbitListener(queues = "${rabbitmq.queue.name}")
+  public void recievedMessage(Employee employee) {
+    System.out.println("Recieved Message From RabbitMQ: " + employee);
+  }
+}
