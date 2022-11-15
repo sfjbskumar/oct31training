5936c612670b4b7dbadef238a9747cf9bab28ade
sfjbs <aratijaware2@gmail.com>
2022-11-15 05:35:46 +0000

    commit

---
+++ Arati/Mini Project Arati/Pension_managment_system/src/test/java/com/example/Pension/Managment/System/PensionManagmentSystemApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.Pension.Managment.System;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PensionManagmentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
+
