bc2815ba97c6fef81133e4bd085e9106f517d25b
sfjbs <sanika.dalvi2000@gmail.com>
2022-11-16 14:56:43 +0000

    Added issue pension and load pension

--- SanikaDalvi/Mini-project/PensionManagementSystem/src/main/java/com/example/PensionManagementSystem/model/Pension.java
+++ SanikaDalvi/Mini-project/PensionManagementSystem/src/main/java/com/example/PensionManagementSystem/model/Pension.java
@@ -1,15 +1,149 @@
 package com.example.PensionManagementSystem.model;
 
-import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Column;
 
-@Entity
+@Entity(name = "pensiondetails")
+@Table(name = "pensiondetails")
 public class Pension {
     @Id
-    @GeneratedValue
+    private int id;
     @Column
+    private String name;
+    @Column
+    private int age;
+    @Column
+    private int balance;
+    @Column
+    private String mobile;
+    @Column
+    private String empStatus;
+    @Column
+    private String pensionStatus;
+    @Column
+    private String pensionMMYY;
+    @Column
+    private int installment;
+
+    public Pension(int id, String name, int age, int balance, String mobile, String empStatus, String pensionStatus, String pensionMMYY, int installment) {
+    }
+
+    public Pension() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getEmpStatus() {
+        return empStatus;
+    }
+
+    public void setEmpStatus(String empStatus) {
+        this.empStatus = empStatus;
+    }
+
+    public int getInstallment() {
+        return installment;
+    }
+
+    public String getPensionStatus() {
+        return pensionStatus;
+    }
+
+    public void setPensionStatus(String pensionStatus) {
+        this.pensionStatus = pensionStatus;
+    }
+
+    public void setInstallment(int installment) {
+        this.installment = installment;
+    }
+
+    public String getPensionMMYY() {
+        return pensionMMYY;
+    }
+
+    public void setPensionMMYY(String pensionMMYY) {
+        this.pensionMMYY = pensionMMYY;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*package com.example.PensionManagementSystem.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity(name="pensiondetails")
+@Table(name="pensiondetails")
+public class Pension {
+    @Id
     private int id;
     @Column
     private String name;
@@ -100,4 +234,4 @@ public Pension(){
         this.pensionmmyy = pensionmmyy;
         this.installment = installment;
     }
-}
+} */
