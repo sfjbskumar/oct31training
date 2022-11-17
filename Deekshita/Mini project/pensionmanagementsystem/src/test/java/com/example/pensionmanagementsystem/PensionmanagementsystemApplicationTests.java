d908c17b7b6bcbaffb0582a62e7a4f3d4e6d7d05
Deekshita535 <117061932+Deekshita535@users.noreply.github.com>
2022-11-17 15:47:52 +0530

    Final commit

--- Deekshita/Mini project/pensionmanagementsystem/src/test/java/com/example/pensionmanagementsystem/PensionmanagementsystemApplicationTests.java
+++ Deekshita/Mini project/pensionmanagementsystem/src/test/java/com/example/pensionmanagementsystem/PensionmanagementsystemApplicationTests.java
@@ -4,8 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class PensionmanagementsystemApplicationTests {
+class PensionManagmentSystemApplicationTests {
 
-
+    @Test
+    void contextLoads() {
+    }
 
 }
