e5f703f79dff8d5d09fbfa5fed964a2459ec56da
sfjbs <mishra99.anjali@gmail.com>
2022-11-20 16:16:16 +0000

    finalCommit

--- anjali/MiniProject/projectmini/src/main/java/com/example/projectMini/Mini/PensionService.java
+++ anjali/MiniProject/projectmini/src/main/java/com/example/projectMini/Mini/PensionService.java
@@ -1,17 +1,91 @@
-<<<<<<< HEAD:Deekshita/Mini project/pensionmanagementsystem/src/main/java/com/example/pensionmanagementsystem/PensionService.java
-package com.example.pensionmanagementsystem.controller;
-=======
 package com.example.projectMini.Mini;
->>>>>>> 2d2de500bdefa65a0aa0ff784c14ee3bb5b9d741:anjali/MiniProject/projectmini/src/main/java/com/example/projectMini/Mini/PensionService.java
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import javax.transaction.Transactional;
+import java.text.SimpleDateFormat;
+//import java.util.ArrayList;
+import java.util.Date;
+//import java.util.List;
 
 @Service
-<<<<<<< HEAD:Deekshita/Mini project/pensionmanagementsystem/src/main/java/com/example/pensionmanagementsystem/PensionService.java
-
+@Transactional
 public class PensionService {
-=======
-public class PensionService {
+    @Autowired
+    PensionRepository pensionRepository;
 
->>>>>>> 2d2de500bdefa65a0aa0ff784c14ee3bb5b9d741:anjali/MiniProject/projectmini/src/main/java/com/example/projectMini/Mini/PensionService.java
+
+    public Pension save(Pension pension)
+    {
+        return  pensionRepository.save(pension);
+
+    }
+    public String checkStatus(int id)
+    {
+        Pension pension=new Pension();
+        pension = pensionRepository.findById(id).get();
+        pension.getEmpStatus();
+        pension.getPensionStatus();
+        return "Employee Status is:-"+pension.getEmpStatus()+" Pension Status is:-"+pension.getPensionStatus();
+
+    }
+    public String checkBalance(int id)
+    {
+        Pension pension=new Pension();
+        pension = pensionRepository.findById(id).get();
+        pension.getBalance();
+        return "Employee Balance is:-"+pension.getBalance();
+    }
+
+    public Pension getApplicantById(int id)
+    {
+        return pensionRepository.findById(id).get();
+    }
+
+    //issuePension to all applicants with empStatus R and PensionStatus Y
+    public void issuePension(){
+        //current MMYY
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MMyy");
+        String currentMMYY = simpleDateFormat.format(new Date());
+
+        SimpleDateFormat monthFormat = new SimpleDateFormat("MM");
+        String currentMonth=monthFormat.format(new Date());
+        SimpleDateFormat yearFormat = new SimpleDateFormat("yy");
+        String currentYear=yearFormat.format(new Date());
+
+        //previous MMYY
+        int prevMonth = Integer.parseInt(currentMonth)-1;
+        String previousMMYY = String.valueOf(prevMonth)+currentYear;
+        if(previousMMYY.length()==3){
+            previousMMYY = "0"+previousMMYY;
+        }
+
+
+        pensionRepository.issuePension("R","Y", currentMMYY, previousMMYY);
+    }
+
+
+    //loadPension to all applicants with empStatus A and PensionStatus N
+    public void loadPension(){
+        //current MMYY
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MMyy");
+        String currentMMYY = simpleDateFormat.format(new Date());
+
+        SimpleDateFormat monthFormat = new SimpleDateFormat("MM");
+        String currentMonth=monthFormat.format(new Date());
+        SimpleDateFormat yearFormat = new SimpleDateFormat("yy");
+        String currentYear=yearFormat.format(new Date());
+
+        //previous MMYY
+        int prevMonth = Integer.parseInt(currentMonth)-1;
+        String previousMMYY = String.valueOf(prevMonth)+currentYear;
+        if(previousMMYY.length()==3){
+            previousMMYY = "0"+previousMMYY;
+        }
+
+        pensionRepository.loadPension("A","N", currentMMYY, previousMMYY);
+    }
+
+
+
 }
