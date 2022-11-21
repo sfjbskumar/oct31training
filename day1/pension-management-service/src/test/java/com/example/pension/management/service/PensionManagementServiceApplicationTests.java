e29ec22827b33a1c33973f9631bff216d2fc303d
naz-22 <117144600+naz-22@users.noreply.github.com>
2022-11-21 13:28:01 +0000

    Mini Project final commit

---
+++ day1/pension-management-service/src/test/java/com/example/pension/management/service/PensionManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pension.management.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PensionManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
