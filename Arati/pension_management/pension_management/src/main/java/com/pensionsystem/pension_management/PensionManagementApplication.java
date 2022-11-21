ac989c44ae0c22eaddb8a3078bcf661b2cb12e75
sfjbs <aratijaware2@gmail.com>
2022-11-21 04:06:50 +0000

    mini project

---
+++ Arati/pension_management/pension_management/src/main/java/com/pensionsystem/pension_management/PensionManagementApplication.java
@@ -0,0 +1,13 @@
+package com.pensionsystem.pension_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PensionManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PensionManagementApplication.class, args);
+	}
+
+}
