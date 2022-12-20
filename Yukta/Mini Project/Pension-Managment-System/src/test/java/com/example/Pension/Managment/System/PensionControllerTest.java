96043f5f6ded5bad7e5a4ea009898f079813146b
Hetik-Chandaria <67996528+hetik-chandaria@users.noreply.github.com>
2022-12-20 07:07:09 +0000

    Add files via upload

---
+++ Yukta/Mini Project/Pension-Managment-System/src/test/java/com/example/Pension/Managment/System/PensionControllerTest.java
@@ -0,0 +1,141 @@
+package com.example.Pension.Managment.System;
+
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.Assert.assertEquals;
+
+@WebMvcTest(PensionController.class)
+class PensionControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private PensionService pensionService;
+
+    @MockBean
+    private PensionRepository pensionRepository;
+
+    @Test
+    @WithMockUser(roles = "ADMIN")
+    void testCreateApplicant() throws Exception{
+        Pension pension = new Pension();
+        pension.setId(201);
+        pension.setName("Yukta");
+        pension.setAge(22);
+        pension.setBalance(90000);
+        pension.setMobile("9999999999");
+        pension.setEmpStatus("A");
+        pension.setPensionStatus("N");
+        pension.setPensionMMYY("0922");
+        pension.setInstallment(980);
+
+        String inputInJson = this.mapToJson(pension);
+
+        String URI = "/create/applicant";
+
+        Mockito.when(pensionService.save(Mockito.any(Pension.class))).thenReturn(pension);
+        RequestBuilder requestBuilder = MockMvcRequestBuilders
+                .post(URI)
+                .accept(MediaType.APPLICATION_JSON).content(inputInJson)
+                .contentType(MediaType.APPLICATION_JSON);
+
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+        MockHttpServletResponse response = result.getResponse();
+
+        String outputInJson = response.getContentAsString();
+
+        assertThat(outputInJson).isEqualTo(inputInJson);
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
+
+    }
+
+    @Test
+    @WithMockUser(roles = "USER")
+    void testCheckBalance() throws Exception{
+
+        Pension pension = new Pension();
+        //  pension.setId(201);
+        pension.setBalance(90000);
+
+        String inputInJson = this.mapToJson(pension.getBalance());
+
+        String URI = "/checkBalance/201";
+
+        Mockito.when(pensionService.checkBalance(201)).thenReturn(String.valueOf(pension.getBalance()));
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get(URI);
+
+
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+        MockHttpServletResponse response = result.getResponse();
+
+        String outputInJson = response.getContentAsString();
+
+        assertThat(outputInJson).isEqualTo(inputInJson);
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
+
+    }
+
+    @Test
+    @WithMockUser(roles = "USER")
+    void testCheckApplicant() throws Exception{
+
+        Pension pension = new Pension();
+        pension.setId(201);
+        pension.setName("Yukta");
+        pension.setAge(22);
+        pension.setBalance(90000);
+        pension.setMobile("9999999999");
+        pension.setEmpStatus("A");
+        pension.setPensionStatus("N");
+        pension.setPensionMMYY("0922");
+        pension.setInstallment(980);
+
+
+        String inputInJson = this.mapToJson(pension);
+
+        String URI = "/checkApplication/201";
+
+        Mockito.when(pensionService.getApplicantById(201)).thenReturn(pension);
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get(URI);
+
+
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+        MockHttpServletResponse response = result.getResponse();
+
+        String outputInJson = response.getContentAsString();
+
+        assertThat(outputInJson).isEqualTo(inputInJson);
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
+
+    }
+
+
+
+
+    /**
+     * Maps an Object into a JSON String. Uses a Jackson ObjectMapper.
+     */
+    private String mapToJson(Object object) throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        return objectMapper.writeValueAsString(object);
+    }
+
+}
---
+++ Yukta/Mini Project/Pension-Managment-System/src/test/java/com/example/Pension/Managment/System/PensionServiceTest.java
@@ -0,0 +1,132 @@
+package com.example.Pension.Managment.System;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+import org.assertj.core.api.Assertions;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class PensionServiceTest{
+
+    @Autowired
+    private PensionService pensionService;
+    @MockBean
+    private PensionRepository pensionRepository;
+
+
+    @Test
+    void testsave(){
+
+        Pension p = new Pension();
+        p.setId(201);
+        p.setName("Yukta");
+        p.setAge(22);
+        p.setBalance(90000);
+        p.setMobile("9999999999");
+        p.setEmpStatus("A");
+        p.setPensionStatus("N");
+        p.setPensionMMYY("0922");
+        p.setInstallment(980);
+        pensionRepository.save(p);
+        Assertions.assertThat(p.getId()).isPositive();
+        verify(pensionRepository, times(1)).save(any(Pension.class));
+    }
+
+    @Test
+    void testGetStatus(){
+        int id=201;
+        Pension p = new Pension();
+        p.setId(id);
+        p.setName("Yukta");
+        p.setAge(22);
+        p.setBalance(80000);
+        p.setMobile("9999999999");
+        p.setEmpStatus("A");
+        p.setPensionStatus("N");
+        p.setPensionMMYY("0922");
+        p.setInstallment(1000);
+        //pensionRepository.getBalance();
+        when(pensionRepository.getEmpStatus(id)).thenReturn("A");
+        assertEquals("A",pensionRepository.getEmpStatus(id));
+        when(pensionRepository.getPensionStatus(id)).thenReturn("N");
+        assertEquals("N",pensionRepository.getPensionStatus(id));
+    }
+
+
+    @Test
+    void testGetBalance(){
+        int id=201;
+        Pension pensionBuilder = new Pension();
+        pensionBuilder.setId(id);
+        pensionBuilder.setName("Tina");
+        pensionBuilder.setAge(22);
+        pensionBuilder.setBalance(80000);
+        pensionBuilder.setMobile("9999999999");
+        pensionBuilder.setEmpStatus("A");
+        pensionBuilder.setPensionStatus("N");
+        pensionBuilder.setPensionMMYY("0922");
+        pensionBuilder.setInstallment(1000);
+
+        Pension p = new Pension();
+        when(pensionRepository.findById(id)).thenReturn(Optional.of(p));
+        Pension pensioner = pensionService.getApplicantById(id);
+        assertEquals(p.getId(), pensioner.getId());
+        assertEquals(p.getName(), pensioner.getName());
+        assertEquals(p.getAge(), pensioner.getAge());
+        assertEquals(p.getBalance(), pensioner.getBalance());
+        assertEquals(p.getMobile(), pensioner.getMobile());
+        assertEquals(p.getEmpStatus(), pensioner.getEmpStatus());
+        assertEquals(p.getPensionStatus(), pensioner.getPensionStatus());
+        assertEquals(p.getPensionMMYY(), pensioner.getPensionMMYY());
+        assertEquals(p.getInstallment(), pensioner.getInstallment());
+
+    }
+    @Test
+    void testGetApplicantById(){
+        int id=201;
+        Pension pensionBuilder = new Pension();
+        pensionBuilder.setId(id);
+        pensionBuilder.setName("Dia");
+        pensionBuilder.setAge(22);
+        pensionBuilder.setBalance(80000);
+        pensionBuilder.setMobile("9999999999");
+        pensionBuilder.setEmpStatus("A");
+        pensionBuilder.setPensionStatus("N");
+        pensionBuilder.setPensionMMYY("0922");
+        pensionBuilder.setInstallment(1000);
+
+        Pension p = new Pension();
+        when(pensionRepository.findById(id)).thenReturn(Optional.of(p));
+        Pension pensioner = pensionService.getApplicantById(id);
+        assertEquals(p.getId(), pensioner.getId());
+        assertEquals(p.getName(), pensioner.getName());
+        assertEquals(p.getAge(), pensioner.getAge());
+        assertEquals(p.getBalance(), pensioner.getBalance());
+        assertEquals(p.getMobile(), pensioner.getMobile());
+        assertEquals(p.getEmpStatus(), pensioner.getEmpStatus());
+        assertEquals(p.getPensionStatus(), pensioner.getPensionStatus());
+        assertEquals(p.getPensionMMYY(), pensioner.getPensionMMYY());
+        assertEquals(p.getInstallment(), pensioner.getInstallment());
+
+    }
+
+    @Test
+    void testLoadPension(){
+        pensionService.loadPension();
+        verify(pensionRepository).loadPension(any(),any(),any(),any());
+    }
+
+    @Test
+    void testIssuePension(){
+        pensionService.issuePension();
+        verify(pensionRepository).issuePension(any(),any(),any(),any());
+    }
+
+
+}
